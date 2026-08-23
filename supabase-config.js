aef2d6ded56cd2a12559db7c3b4499a886ab963c
jackplaygames1-design <jackplaygames1@gmail.com>
2026-08-23 02:52:23 -0400

    Connect portfolio editor to restored Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 window.SUPABASE_CONFIG = Object.assign(
   {
-    url: "https://qcfdionrpetayqawvjrt.supabase.co",
-    anonKey: "sb_publishable_gdsHK0H1YOZahDX9ST8oJg_rKeMp8p8",
+    url: "https://ufehiibiaafwddzaxpxf.supabase.co",
+    anonKey: "sb_publishable_ltaNA7nnVozoSCOcZIjg",
     projectTable: "portfolio_projects",
     storageBucket: "portfolio-media",
     publicSection: "art",
