9cd7bd649cb152445dd4baee714d376c3e55ba6f
jackplaygames1-design <jackplaygames1@gmail.com>
2026-08-23 02:54:26 -0400

    Use Supabase browser-compatible anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 window.SUPABASE_CONFIG = Object.assign(
   {
     url: "https://ufehiibiaafwddzaxpxf.supabase.co",
-    anonKey: "sb_publishable_ltaNA7nnVozoSCOcZIjg",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmZWhpaWJpYWFmd2RkemF4cHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0NTkxNDEsImV4cCI6MjEwMzAzNTE0MX0.qT4PYW4nQiSu5nhiFwW5UNJ0wS695QCPHK7LmnKMIeY",
     projectTable: "portfolio_projects",
     storageBucket: "portfolio-media",
     publicSection: "art",
